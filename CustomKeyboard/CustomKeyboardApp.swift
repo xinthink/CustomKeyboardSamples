333fa49105a11ab61b985bafd2c6b0fd52d995ca
Think Wu <yingxinwu.g@gmail.com>
2025-03-25 14:12:43 +0800

    Initial Commit

---
+++ CustomKeyboard/CustomKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomKeyboardApp.swift
+//  CustomKeyboard
+//
+//  Created by Yingxin Wu on 2025/3/4.
+//
+
+import SwiftUI
+
+@main
+struct CustomKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CustomKeyboardTests/CustomKeyboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  CustomKeyboardTests.swift
+//  CustomKeyboardTests
+//
+//  Created by Yingxin Wu on 2025/3/4.
+//
+
+import Testing
+@testable import CustomKeyboard
+
+struct CustomKeyboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
